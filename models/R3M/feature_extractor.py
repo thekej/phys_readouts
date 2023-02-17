d0648898f7e234e1fb3edbc527a01cb2cdf04f22
Khaled Jedoui <thekej@node6-ccn2cluster.stanford.edu>
2023-02-16 23:05:56 -0800

    r3m works

--- models/R3M/feature_extractor.py
+++ models/R3M/feature_extractor.py
@@ -17,7 +17,7 @@ def main(args):
                                     n_past=7, 
                                     full_rollout=True)
     model = load_model(model, args.model_path)
-    
+    model = model.to('cuda') 
     '''
     output = {
             "input_states": input_states,
@@ -29,24 +29,28 @@ def main(args):
     print('load data')
     dataset = R3MDataset(args.data_path)
     loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
-    
+    data_size = 10551
+    n_features = 10
     # set up new dataset
     f = h5py.File(args.save_file, "w")
     dset1 = f.create_dataset("label", (data_size,), dtype='f')
     dset2 = f.create_dataset("observed", (data_size, n_features, 16, 16), dtype='f')
     dset3 = f.create_dataset("observed_full_outcome", (data_size, n_features, 8, 8, 256), dtype='f')
-    dset4 = f.create_dataset("simulation", (data_size, n_features - args.open_loop_ctx, 8, 8, 256), dtype='f')
+    dset4 = f.create_dataset("simulation", (data_size, n_features, 8, 8, 256), dtype='f')
 
 
     for i, batch in enumerate(tqdm.tqdm(loader)):
         v_in, label_in = batch
-
+        v_in = v_in.to('cuda')
+        
         if (i+1)*args.batch_size < data_size:
             dset1[i*args.batch_size:(i+1)*args.batch_size] = label_in.reshape(-1)
         else:
             dset1[i*args.batch_size:] = label_in.reshape(-1)
         # input is (Bs, T, 3, H, W)
         output = model(v_in)
+        print(output)
+        exit()
         if (i+1)*args.batch_size < data_size:#(8, 4, 50, 16, 16) (8, 4, 50, 8, 8, 256) (8, 4, 5, 8, 8, 256)
             dset2[i*args.batch_size:(i+1)*args.batch_size] = x.reshape(-1, n_features, 16, 16)
             dset3[i*args.batch_size:(i+1)*args.batch_size] = z.reshape(-1, n_features, 8, 8, 256)
@@ -61,16 +65,13 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ckpt', type=str, required=True)
-    parser.add_argument('--vqvae_ckpt', type=str, required=True)
+    parser.add_argument('--model_path', type=str, required=True)
+    parser.add_argument('--encoder_name', type=str, required=True)
+    parser.add_argument('--dynamics_name', type=str, required=True)
     parser.add_argument('--data_path', type=str, required=True)
     parser.add_argument('--save_file', type=str, required=True)
-    parser.add_argument('--embeddings', type=str, required=True)
-    parser.add_argument('--scenario', type=str, default='past', required=True)
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--seq_len', type=int, default=45)
-    parser.add_argument('--open_loop_ctx', type=int, default=None)
-    parser.add_argument('--seed', type=int, default=0)
     args = parser.parse_args()
 
     main(args)
+
