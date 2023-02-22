723e88095d90aa6898319df6642b135ba12fc3e1
Khaled Jedoui <thekej@stanford.edu>
2023-02-22 15:29:32 -0800

    add data loader + balance r3m data

--- utils/readout_feats_loader.py
+++ utils/readout_feats_loader.py
@@ -26,3 +26,22 @@ class FeaturesDataset(Dataset):
             x = x[0]
         y = self.labels[index].astype(np.float32)
         return x, y
+    
+class R3MFeaturesDataset(Dataset):
+    def __init__(self, h5_path, set_ = None, scenario='observed'):
+        self.indices = set_
+        data = h5py.File(h5_path)
+        self.features = data[scenario]
+        self.labels = data['label']
+
+    def __len__(self):
+        if not self.indices is None:
+            return len(self.indices)
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        if not self.indices is None:
+            index = self.indices[index]
+        x = self.features[index]
+        y = self.labels[index].astype(np.float32)
+        return x, y
