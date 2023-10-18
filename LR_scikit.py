1715abec2feaa79ceaf43c19f1a797af8f179c93
Khaled Jedoui <thekej@stanford.edu>
2023-10-17 23:26:04 -0700

    Update LR_scikit.py

--- LR_scikit.py
+++ LR_scikit.py
@@ -27,7 +27,7 @@ def one_scenario_eval(args, model, data, target, scenario, result, indices):
         stimulus = {v: k for k, v in stimulus_.items()}
         
     accuracy = model.score(data, target)
-    result[scenario+'_test'] = accuracy
+    result[scenario+'_test'] = f"{accuracy:.4f}"
     print(f"Accuracy on %s test data (%d data points): {accuracy:.4f}"%(scenario, target.shape[0]))
     probs = model.predict_proba(data)
     preds = model.predict(data)
@@ -36,7 +36,7 @@ def one_scenario_eval(args, model, data, target, scenario, result, indices):
               'Predicted Prob_true', 'Predicted Outcome', 'Actual Outcome', 
               'Stimulus Name']]
     for i in range(target.shape[0]):
-        entry = [scenario, scenario, float(result['train']), float(result[scenario+'_test']),
+        entry = [scenario, args.data_type, scenario, float(result['train']), float(result[scenario+'_test']),
                  args.scenario_name, float(probs[i][0]), float(probs[i][1]), int(preds[i]),
                  int(target[i]), stimulus[indices[i]]]
         results.append(entry)
@@ -53,12 +53,12 @@ def all_scenario_eval(args, model, data, target, scenario, result, indices, resu
         stimulus = {v: k for k, v in stimulus_.items()}
         
     accuracy = model.score(data, target)
-    result[scenario+'_test'] = accuracy
+    result[scenario+'_test'] = f"{accuracy:.4f}"
     print(f"Accuracy on %s test data (%d data points): {accuracy:.4f}"%(scenario, target.shape[0]))
     probs = model.predict_proba(data)
     preds = model.predict(data)
     for i in range(target.shape[0]):
-        entry = ['all', scenario, float(result['train']), float(result[scenario+'_test']),
+        entry = ['all', args.data_type, scenario, float(result['train']), float(result[scenario+'_test']),
                  args.scenario_name, float(probs[i][0]), float(probs[i][1]), int(preds[i]),
                  int(target[i]), stimulus[indices[i]]]
         results.append(entry)
@@ -70,10 +70,10 @@ def test_model(model, test_data, test_label, args, result):
         test_data = test_data.reshape(test_data.shape[0], -1)
         accuracy = model.score(test_data, test_label)
         print(f"Accuracy on full test data: {accuracy:.4f}")
-        result['full_test'] = accuracy
+        result['full_test'] = f"{accuracy:.4f}"
         scenarios_indices = json.load(f)
         
-        results = [['Readout Train Data', 'Readout Test Data', 'Train Accuracy', 
+        results = [['Readout Train Data', 'Model', 'Readout Test Data', 'Train Accuracy', 
               'Test Accuracy', 'Readout Type', 'Predicted Prob_false', 
               'Predicted Prob_true', 'Predicted Outcome', 'Actual Outcome', 
               'Stimulus Name']]
@@ -94,14 +94,13 @@ def test_model(model, test_data, test_label, args, result):
             else:
                 ind = sorted(scenarios_indices[sc])
                 accuracy = model.score(test_data[ind], test_label[ind])
-                result[sc+'_test'] = accuracy
+                result[sc+'_test'] = f"{accuracy:.4f}"
                 print(f"Accuracy on %s test data: {accuracy:.4f}"%sc)
-        if args.one_scenario == 'all':
-            filename = args.data_type+'_'+ args.model_type + '_' +args.scenario_name+ \
-            '_only_'+str(args.one_scenario)+'_results.csv'
-            with open(filename, 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerows(results)
+        filename = args.data_type+'_'+ args.model_type + '_' +args.scenario_name+ \
+            '_only_'+str(args.one_scenario)+'_exclude_'+str(args.all_but_one)+'_results.csv'
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(results)
     return result
             
 def get_indices(scenarios_indices):
@@ -135,7 +134,7 @@ def train(args):
                                                                  args.one_scenario))
             
     if args.data_type == 'mcvd':
-        scenario_feature = 'features_mid'
+        scenario_feature = 'features'
     else:
         scenario_feature = args.scenario
     X = load_hdf5(args.data_path, scenario_feature, indices)
@@ -154,7 +153,7 @@ def train(args):
     # Define the hyperparameter grid to search
     print('Load model')
     if args.model_type == 'logistic':
-        param_grid = {'clf__C': np.array([0.01, 0.1, 1, 5, 10, 20]), 'clf__penalty': ['l2']}#  np.logspace(-1, 3, 5), 'clf__penalty': ['l2']}
+        param_grid = {'clf__C': np.array([args.penalty]), 'clf__penalty': ['l2']}#  np.logspace(-1, 3, 5), 'clf__penalty': ['l2']}
         model = LogisticRegression(max_iter=20000)
     elif args.model_type == 'svc':
         param_grid = {'clf__C': np.logspace(-3, 0, 4), 'clf__loss': ['hinge']}
@@ -167,7 +166,7 @@ def train(args):
     print('Grid Search with KFold')
     import time
     t = time.time()
-    stratified_kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=args.random_state)
+    stratified_kfold = StratifiedKFold(n_splits=2, shuffle=True, random_state=args.random_state)
     grid_search = GridSearchCV(pipeline, param_grid, cv=stratified_kfold, verbose=3)
     grid_search.fit(X, y)
     print(time.time()-t)
@@ -178,7 +177,7 @@ def train(args):
     # Evaluate the model on the full data
     accuracy = grid_search.score(X, y)
     print(f"Accuracy on train data: {accuracy:.4f}")
-    result['train'] = accuracy
+    result['train'] = f"{accuracy:.4f}"
     
     #
     test_data = load_hdf5(args.test_path, scenario_feature)
@@ -191,7 +190,9 @@ def train(args):
     test_label = load_hdf5(args.test_path, 'label')
     result = test_model(grid_search, test_data, test_label, args, result)
     
-    filename = args.data_type+'_'+ args.model_type + '_' +args.scenario_name+'_exclude_'+str(args.all_but_one)+'_results.json'     
+    filename = args.data_type+'_'+ args.model_type + '_' +args.scenario_name+'_exclude_'+str(args.all_but_one)+f'_penalty_{args.penalty}_results.json'     
+    print(result)
+    result = {key: str(value) for key, value in result.items()}
 
     with open(filename, 'w') as f:
         json.dump(result, f)
@@ -216,6 +217,7 @@ def main():
     parser.add_argument('--model-type', type=str, choices=['logistic', 'svc'], help='type of model to train (logistic or svc)')
     parser.add_argument('--model-path', type=str, default='best_model.joblib', help='path to save or load the best model')
     parser.add_argument('--random-state', type=int, default=42, help='random seed for reproducibility')
+    parser.add_argument('--penalty', type=float, default=1, help='random seed for reproducibility')
     
     # data params
     parser.add_argument('--data-path', type=str, help='The path to the h5 file')
