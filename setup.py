5a5ef31acf23a271996e4e7753730c01d13a8394
Khaled Jedoui <thekej@node13-ccn2cluster.stanford.edu>
2024-08-16 17:51:49 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -1,21 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='phys_readouts',
+    name='phys_extractors',
     version='0.1',
     packages=find_packages(),  # Automatically finds all packages in the directory
-    install_requires=[
-        # Add any dependencies here
-    ],
-    include_package_data=True,  # Ensure that non-Python files specified in MANIFEST.in are included
-    package_data={
-        # If you need to include non-Python files, specify them here
-        '': ['*.txt', '*.md'],
-        'phys_readouts': ['models/*', 'utils/*'],  # Include all files under models and utils
-    },
-    entry_points={
-        'console_scripts': [
-            # Example: 'cli_name = phys_readouts.module:function',
-        ],
-    },
 )
