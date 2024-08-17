6d2ff4adbe58252bac5b5a6dc27458d5ea6b1c51
Khaled Jedoui <thekej@stanford.edu>
2024-08-16 17:07:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='phys_readouts',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(),  # Automatically finds all packages in the directory
+    install_requires=[
+        # Add any dependencies here
+    ],
+    include_package_data=True,  # Ensure that non-Python files specified in MANIFEST.in are included
+    package_data={
+        # If you need to include non-Python files, specify them here
+        '': ['*.txt', '*.md'],
+        'phys_readouts': ['models/*', 'utils/*'],  # Include all files under models and utils
+    },
+    entry_points={
+        'console_scripts': [
+            # Example: 'cli_name = phys_readouts.module:function',
+        ],
+    },
 )
