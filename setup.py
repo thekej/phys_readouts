dfed95d1ede49828842b324cb0c28df0ccfe8b2f
Khaled Jedoui <thekej@node13-ccn2cluster.stanford.edu>
2024-08-16 18:08:00 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='phys_extractors',
+    name='phys_readouts',
     version='0.1',
     packages=find_packages(),  # Automatically finds all packages in the directory
 )
