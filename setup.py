8a1768c6827e967c8d0ec8bd7f144000820416ec
Khaled Jedoui <thekej@node13-ccn2cluster.stanford.edu>
2024-08-23 19:40:33 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,31 +1,12 @@
-from setuptools import setup, find_packages, Extension
-import os
+from setuptools import setup, find_packages
 
-# Function to find all C++ files in the entire project directory
-def find_cpp_files():
-    cpp_files = []
-    for root, _, files in os.walk('.'):  # Start from the current directory
-        for file in files:
-            if file.endswith(".cpp"):
-                cpp_files.append(os.path.join(root, file))
-    return cpp_files
-
-# Find all C++ files in the entire project
-cpp_files = find_cpp_files()
-
-# Define the extension module
-phys_readouts_extension = Extension(
-    name='phys_readouts_extension',  # Name of the extension module
-    sources=cpp_files,
-    include_dirs=[],  # Add any include directories if needed
-    extra_compile_args=['-std=c++11'],  # Add any compiler flags if needed
-)
-
-# Setup configuration
 setup(
     name='phys_readouts',
     version='0.1',
     packages=find_packages(),  # Automatically finds all packages in the directory
-    ext_modules=[phys_readouts_extension],  # Include the extension module
+    include_package_data=True,  # Include package data specified in MANIFEST.in
+    package_data={
+        '': ['*.cpp'],  # Include all .cpp files in the package
+    },
 )
 
